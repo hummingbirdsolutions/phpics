1a6dc7b77cea7ea2a77de8d89830027420c731cd
Pieter Pottas <pieter.pottas@indlu.io>
2022-05-25 12:09:31 -0500

    Add support for full days in addition to part-day

--- src/index.php
+++ src/index.php
@@ -6,42 +6,84 @@ namespace HummingbirdSolutions\Phpics;
 # https://www.ietf.org/rfc/rfc5545.txt
 final class Index
 {
-    private function dateToIcsDateFormat($time) {
-        return date('Ymd\This', $time) . 'Z';
+    // Example: "2010-02-06 19:30:13";
+    private function sqlDateToIcsDateTimeFormat($mysqldate) {
+        // $mysqldate = "2010-02-06 19:30:13";
+        $phpdate = strtotime( $mysqldate );
+        $icsdate = date ('Ymd\THis', $phpdate);
+        return $icsdate;
     }
 
+    // Example: "2010-02-06 19:30:13";
+    private function sqlDateToIcsDateFormat($mysqldate) {
+        $phpdate = strtotime( $mysqldate );
+        $icsdate = date ('Ymd', $phpdate);
+        return $icsdate;
+    }
+
+    private function encodeIcsText($str) {
+        return addcslashes($str, "\\;,");
+    }
+
+    // Generate a universally unique identifier
+    // Creates a type 4 UUID (aka GUID in Microsoft land)
+    public static function generateUUIDv4()
+    {
+        $data = openssl_random_pseudo_bytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    // address is optional
+    // If datened not supplied, assume 1 full calender day taking into account the startdate parameter
     public function generateIcs(
         $title = "Meeting",
-        $datestart = null, # unix timestamp
-        $dateend = null, # unix timestamp
-        $url = "http://www.hummingbirdsolutions.tech/calenderentry/987",
-        $address = null,
-        $uid = null,
-        $description = null)
+        $description = null,
+        $datestart = null, # mysql date
+        $dateend = null, # mysql date
+        $address = null)
     {
         if (!isset($datestart)) {
             throw new \Exception("The start date is required");
         }
 
-        if (!isset($dateend)) {
-            throw new \Exception("The end date is required");
+        if (!isset($description)) {
+            throw new \Exception("A description is required");
         }
-        
-        # TODO: More validations here...
+
+        if (!isset($title)) {
+            throw new \Exception("A title is required");
+        }
+
+        $tzid = 'America/New_York';
+
+        $icslocation = '';
+
+        if (isset($address)) {
+            $icslocation = '
+LOCATION:' . $this->encodeIcsText($address);
+        }
+
+        $icsdate = '';
+
+        if (isset($dateend)) {
+            $icsdate = 'DTSTART;TZID=' . $tzid . ':' . $this->sqlDateToIcsDateTimeFormat($datestart) . '
+DTEND;TZID=' . $tzid . ':' . $this->sqlDateToIcsDateTimeFormat($dateend);
+        } else {
+            $icsdate = 'DTSTART;TZID=' . $tzid . ':' . $this->sqlDateToIcsDateFormat($datestart);
+        }
 
         $icsFileText = 'BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:-//Hummingbird Solutions//NONSGML PHP ICS//EN
 CALSCALE:GREGORIAN
 BEGIN:VEVENT
-DTSTART:' . $this->dateToIcsDateFormat($datestart) . '
-DTEND:' . $this->dateToIcsDateFormat($dateend) . '
-UID:' . $uid . '
-DTSTAMP:' . time() . '
-LOCATION:' . addslashes($address) . '
-DESCRIPTION:' . addslashes($description) . '
-URL;VALUE=URI:' . $url . '
-SUMMARY:' . addslashes($title) . '
+' . $icsdate . '
+UID:' . $this->generateUUIDv4() . '
+DTSTAMP;TZID=' . $tzid . ':' . date ('Ymd\THis', time()) . $icslocation . '
+DESCRIPTION:' . $this->encodeIcsText($description) . '
+SUMMARY:' . $this->encodeIcsText($title) . '
 END:VEVENT
 END:VCALENDAR
 ';
@@ -52,6 +94,6 @@ END:VCALENDAR
 
 // $ics = new Index();
 
-// echo $ics->generateIcs("Stuff", 99999999, 199999999, "http://www.hummingbirdsolutions.tech/calenderentry/987", "225 Jan Str, Appels, Centurion", "sghfgs", "Dessc");
+// echo $ics->generateIcs("Very important meeting", "Meet with the President", "2010-02-06 19:30:13", "2010-02-06 19:50:13", "1600 Pennsylvania Avenue NW, Washington, DC 20500");
 
 ?>
--- tests/IcsValidationsTest.php
+++ tests/IcsValidationsTest.php
@@ -5,11 +5,80 @@ use HummingbirdSolutions\Phpics\Index;
 
 final class IcsValidationsTest extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress(): void
+    public function testThrowsValidationExceptions(): void
     {
         $a = new Index();
 
         $this->expectException(Exception::class);
         $a->generateIcs();
+    }
+
+    private function testIcsEqual($expect, $actual, $excludeArr) {
+        $expectParts = explode( "\n", $expect );
+        $actualParts = explode( "\n", $actual );
+
+        $arrLen = count($expectParts);
+        for ($x = 0; $x < $arrLen; $x++) {
+            if (in_array($x, $excludeArr)) {
+                continue;
+            }
+
+            $this->assertEquals(
+                $expectParts[$x],
+                $actualParts[$x],
+                "actual value is not equals to expected"
+            );
+        }
+    }
+
+    public function testValidStartEndDate(): void
+    {
+        $ics = new Index();
+        
+        $expect = 'BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Hummingbird Solutions//NONSGML PHP ICS//EN
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+DTSTART;TZID=America/New_York:20220606T193013
+DTEND;TZID=America/New_York:20220606T195013
+UID:8e91754d-4603-40c7-9bc7-07819767ad81
+DTSTAMP;TZID=America/New_York:20220525T170449
+LOCATION:1600 Pennsylvania Avenue NW\, Washington\, DC 20500
+DESCRIPTION:Meet with the President
+SUMMARY:Very important meeting
+END:VEVENT
+END:VCALENDAR
+';
+
+        $actual = $ics->generateIcs("Very important meeting", "Meet with the President", "2022-06-06 19:30:13", "2022-06-06 19:50:13", "1600 Pennsylvania Avenue NW, Washington, DC 20500");
+        // exclude UID and DTSTAMP
+        $excludeArr = [7, 8];
+        $this->testIcsEqual($expect, $actual, $excludeArr);
+    }
+
+    public function testValidFullDay(): void
+    {
+        $ics = new Index();
+        
+        $expect = 'BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Hummingbird Solutions//NONSGML PHP ICS//EN
+CALSCALE:GREGORIAN
+BEGIN:VEVENT
+DTSTART;TZID=America/New_York:20100206
+UID:292597ca-e141-4468-ac31-1fd8dabef7a4
+DTSTAMP;TZID=America/New_York:20220525T170804
+LOCATION:1600 Pennsylvania Avenue NW\, Washington\, DC 20500
+DESCRIPTION:Meet with the President
+SUMMARY:Very important meeting
+END:VEVENT
+END:VCALENDAR
+';
+
+        $actual = $ics->generateIcs("Very important meeting", "Meet with the President", "2010-02-06 19:30:13", null, "1600 Pennsylvania Avenue NW, Washington, DC 20500");
+        // exclude UID and DTSTAMP
+        $excludeArr = [6, 7];
+        $this->testIcsEqual($expect, $actual, $excludeArr);
     }
 }
